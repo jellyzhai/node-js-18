66114dc93ff20d14d36c6eac94eb59bd029e40a7
yuanjie.zhai <yuanjie.zhai@easystack.cn>
2023-01-19 17:53:11 +0800

    express 表单注册1

--- 05_express/index.js
+++ 05_express/index.js
@@ -1,33 +1,49 @@
 const express = require('express')
-const path = require("path");
+const path = require('path')
 
 const app = express()
 
+// 注册的用户数据
+const USERS = []
 
+// 设置静态资源目录
 app.use(express.static(path.resolve(__dirname, './public')))
 
-app.get('/login', (req, res) => {
-    // console.log( '参数：', req.params)
-    // console.log("请求体：", req.body);
-    // console.log("请求头：", req.headers);
-    // console.log("查询对象：", req.query);
+// 解析请求体的数据
+app.use(express.urlencoded())
 
-    if (req.query.username === 'jelly' && req.query.password === '123123') {
-        res.send('登陆成功')
+// get 方法获取表单数据
+app.get('/login', (req, res) => {
+    if (req.query.username === 'admin' && req.query.password === '123123') {
+        res.send('欢迎回来！登陆成功~')
     } else {
         res.send('用户名或密码错误')
     }
 })
 
+app.post('/login', (req, res) => {
+    console.log(req.body)
 
-app.get('/hello', (req, res, next) => {
-    console.log('访问的路径：' + req.url);
-    res.send("<h1>hello</h1>");
-
-    console.log(next);
+    // express 默认不会解析请求体中的数据，需要使用中间件
+    res.send(req.body)
 })
 
+app.post('/registry', (req, res) => {
+    const {username, password, repwd, nickname} = req.body;
+
+    USERS.push({ username, password, repwd, nickname });
+
+    console.log(req.body)
+
+    // express 默认不会解析请求体中的数据，需要使用中间件
+    res.send(req.body)
+})
+
+// get 方法获取数据的第二种方式
+app.get('/hello/:id', (req, res) => {
+    res.send(req.params)
+})
 
 app.listen(3000, () => {
-    console.log("http://127.0.0.1:3000 服务器启动成功");
+    console.log('服务器启动成功：http://127.0.0.1:3000');
 })
